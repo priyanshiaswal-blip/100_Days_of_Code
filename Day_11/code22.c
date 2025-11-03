822424e100f6dc0d018f70e6f646621476883ca2
Priyanshi Aswal <priyanshiaswal01@gmail.com>
2025-11-03 23:29:51 +0530

    Add files via upload

---
+++ Day_11/code22.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    float CP, SP, profit, loss, profit_percentage, loss_percentage;
+    
+    //input the cost price and selling price
+    printf("Enter the cost price and selling price: ");
+    scanf("%f %f", &CP, &SP);
+
+    //
+    profit = SP-CP;
+    loss = CP-SP;
+
+    profit_percentage = (profit / CP) * 100;
+    loss_percentage = (loss / CP) * 100;
+
+    if(SP > CP)
+    {
+        printf("Profit\n");
+        printf("Profit percentage = %.0f %%", profit_percentage);
+    }
+
+    else if(SP < CP)
+    {
+        printf("Loss\n");
+        printf("Loss percenatge = %.0f %%", loss_percentage);
+    }
+
+    else
+    {
+        printf("It is neither Profit nor Loss.");
+    }
+
+    return 0;
+}
